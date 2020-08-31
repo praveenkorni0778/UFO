8d80d25758d8d3d369785fca88d858c540493229
Praveen Korni <kornipraveen78@gmail.com>
2020-08-31 21:56:12 +0530

    Add project files.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Text CountText;
+    public Text WinText;
+    private Rigidbody2D rb2d;
+    private int count;
+
+    public float speed;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        WinText.text = "";
+        SetCountText();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        CountText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            WinText.text = "You Win!";
+        }
+    }
+}
